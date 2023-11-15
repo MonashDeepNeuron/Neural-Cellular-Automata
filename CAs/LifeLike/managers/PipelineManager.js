ecef9c86d5319a537a6f901a95b77e286583d866
Joshua-Riantoputra <jria0003@student.monash.edu>
2023-11-15 18:10:13 +1100

    made correct folder structure. began refactoring code

---
+++ CAs/LifeLike/managers/PipelineManager.js
@@ -0,0 +1,47 @@
+class PipelineManager {
+    // SIMULATION PIPELINE
+    static simulationPipeline = DeviceManager.getDevice().createComputePipeline({
+        label: "Simulation pipeline",
+        layout: pipelineLayout,
+        compute: {
+            module: simulationShaderModule,
+            entryPoint: "computeMain",
+        }
+    });
+
+    static cellPipeline = device.createRenderPipeline({
+        label: "Cell pipeline",
+        layout: pipelineLayout,
+        vertex: {
+            module: cellShaderModule,
+            entryPoint: "vertexMain",
+            buffers: [vertexBufferLayout],
+        },
+        fragment: {
+            module: cellShaderModule,
+            entryPoint: "fragmentMain",
+            targets: [{
+                format: canvasFormat
+            }],
+        }
+    });
+    static pipelines = new Map();
+
+    static createRenderPipeline(device, label, layout, vertexInfo, fragmentInfo, canvasFormat) {
+        const pipeline = DeviceManager.getDevice().createRenderPipeline({
+            label,
+            layout,
+            vertex: vertexInfo,
+            fragment: fragmentInfo,
+            targets: [{ format: canvasFormat }],
+        });
+
+        this.pipelines.set(label, pipeline);
+        return pipeline;
+    }
+
+    static getPipeline(label) {
+        return this.pipelines.get(label);
+    }
+
+}
