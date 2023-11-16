6565bd9137a7af3d9351ef52d2743688329e70d6
Joshua-Riantoputra <jria0003@student.monash.edu>
2023-11-16 22:00:01 +1100

    my web dev friend showed me this cool thing

---
+++ CAs/LifeLike/main.js
@@ -0,0 +1,12 @@
+// imports
+import main from "./life.js";
+import EventManager from "./managers/EventManager.js";
+
+// Setting up event bindings
+document.getElementById('play').addEventListener('click', EventManager.playPause);  // play pause button
+document.getElementById('next').addEventListener('click', EventManager.moveOneFrame); // move one frame button
+document.getElementsByTagName("body")[0].addEventListener("keydown", EventManager.keyListener); // key presses
+document.getElementById('submitInput').addEventListener('click', EventManager.updateRuleString); // new rule string input button
+
+// run cellular automata
+main()
