0eee57be1f3282a8046960b2d369a6dc16453a91
Joshua-Riantoputra <jria0003@student.monash.edu>
2023-11-23 09:18:00 +1100

    im going to start crying GPU setup is so hard

--- Understand webGPU/main.js
+++ Understand webGPU/main.js
@@ -1,6 +1,3 @@
-// wrong bad ugly imports
-import vertex from "./vertex.wgsl"
-
 // synopsis: canvas, device, texture, buffer, bindings, pipeline, encoder
 // SET UP 
 
@@ -21,11 +18,11 @@ context.configure({
 // TEXTURE; buffer with special operations (good for 2D and 3D data)
 const texture = context.getCurrentTexture(); // basic
 
-// BUFFER;  is raw binary data that gets sent to the gpu
+// BUFFER; raw binary data that gets sent to the gpu
 
 // write predetermined vertices to buffer
 // declare information that will be written
-const vertices = new Float32Array([
+const vertices = new Float32Array([ // whats with this new shit?
     // X,    Y,
     -0.8, -0.8, // Triangle 1
     -0.8, 0.8,
@@ -36,7 +33,7 @@ const vertices = new Float32Array([
 const vertexBuffer = device.createBuffer({
     label: "a triangle",
     size: vertices.byteLength,
-    usage: GPUBufferUsage.VERTEX, // vertex buffers are kinda special
+    usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST, // vertex buffers are kinda special
 });
 
 // put the binary information into the buffer
@@ -51,7 +48,7 @@ const bindGroupLayout = device.createBindGroupLayout({
     entries: [{
         binding: 0,
         visibility: GPUShaderStage.VERTEX, // the gpu sees it at different stages depending on flag
-        buffer: vertexBuffer,
+        buffer: {},
     }],
 });
 const bindGroup = device.createBindGroup({
@@ -63,19 +60,44 @@ const bindGroup = device.createBindGroup({
     }], // comma for consistency i guess
 });
 
+// SHADER, WILL BE REMOVED WHEN THE PROGRAM WORKS
+const shaders = device.createShaderModule({
+    label: "shaders",
+    code:
+        /*wgsl*/`
+        @vertex
+        fn vertexMain(@location(0) pos: vec2f) ->
+          @builtin(position) vec4f {
+          return vec4f(pos, 0, 1);
+        }
+
+        @fragment
+        fn fragmentMain() -> @location(0) vec4f {
+            return vec4f(1, 0, 0, 1); // (Red, Green, Blue, Alpha)
+        }`,
+});
+
+
 // PIPELINE CREATION; specifies the bindings and modules to the GPU
+/*
 const pipelineLayout = device.createPipelineLayout({
     label: "basic layout",
     entries: { bindGroupLayouts: [bindGroupLayout] }
 });
+*/
 const pipeline = device.createRenderPipeline({
     label: "main pipeline",
-    layout: pipelineLayout,
+    layout: "auto",
     vertex: { // FILL THIS IN
-        module: 0,
-        entryPoint: 0,
-        buffer: 0,
-    }, // necessary, this is the .wgsl module which I will write later
+        module: shaders,
+        entryPoint: "vertexMain", // not this
+        buffer: 0, // not this(?)
+    },
+    fragment: {
+        module: shaders,
+        entryPoint: "fragmentMain", // not this
+        targets: [{ format: format }], // not this(?)
+    }
 }); // p
 
 // ENCODER; queues instructions to the GPU via command buffer
@@ -84,11 +106,12 @@ const pipeline = device.createRenderPipeline({
 const commandEncoder = device.createCommandEncoder();
 const renderPass = commandEncoder.beginRenderPass({
     label: "pass encoder",
-    colorAttachment: {
-        loadOp: "clear", // better for performance than load.
-        storeOp: "store", // try discard later
+    colorAttachments: [{
         view: texture.createView(), // to view the texture
-    },
+        loadOp: "clear", // better for performance than load.
+        clearValue: { r: 0, g: 0, b: 0.4, a: 1.0 }, // what is empty
+        storeOp: "store", // try discard later
+    }],
 });
 
 renderPass.setPipeline(pipeline);
@@ -97,3 +120,28 @@ renderPass.draw(3, 1);
 renderPass.end();
 
 device.queue.submit([commandEncoder.finish()]);
+
+
+/*
+setup
+// Device, Canvas, Context // setupManager (might not be a class)
+// Textures, Buffers, Bindings // Resource Manager
+// Pipeline // PipelineManager
+
+
+// all event manager things
+detect play click -> starts looping (with set interval() )
+detect pause click -> stops the loop (actually fully stopped) (clearInterval() )
+detect one frame button -> executes oneFrame() 
+detect rule string submission -> write to buffer
+speed -> changes interval
+
+// maybe the encoder manager handles this
+oneFrame(a,b,c) { 
+    encoder
+    render pass
+    submit
+    return Null
+}; 
+*/
+
