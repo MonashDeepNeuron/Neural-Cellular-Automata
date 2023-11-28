e9b7401ecb22c7b49bdd20295494d5242a4c2059
KCollins446 <kerenmcollins@gmail.com>
2023-11-29 02:23:11 +1100

    Feel free to revert bc not sure it is improvement Did some funky stuff with the update loop and encoder pass

--- CAs/LifeLike/managers/EventManager.js
+++ CAs/LifeLike/managers/EventManager.js
@@ -8,6 +8,8 @@ export default class EventManager {
     static newRuleString = false;
     static ruleString = "/2";
     static updateInterval = 50;
+    static currentTimer = 0; // Update interval
+    static updateLoop = () => {};
 
     // key bindings
     static PLAY_PAUSE_KEY = 'k';
@@ -51,5 +53,20 @@ export default class EventManager {
         console.log(inputSpeed);
         const newUpdateInterval = 50 + (2 * (100 - inputSpeed));
         EventManager.updateInterval = newUpdateInterval;
+    }
+
+    static bindEvents(){
+        document.getElementById('play').addEventListener('click', EventManager.playPause);  // play pause button
+        document.getElementById('next').addEventListener('click', EventManager.moveOneFrame); // move one frame button
+        document.getElementsByTagName("body")[0].addEventListener("keydown", EventManager.keyListener); // key presses
+        document.getElementById('submitInput').addEventListener('click', EventManager.updateRuleString); // new rule string input button
+        document.getElementById('speedInput').addEventListener('click', () => {
+        EventManager.updateSpeed();
+        clearInterval(EventManager.currentTimer);
+        EventManager.currentTimer = setInterval(EventManager.updateLoop, EventManager.updateInterval)
+        console.log(`The EventManager.currentTimer is ${EventManager.currentTimer}`)
+    }
+
+    ); // change speed
     }
 }
