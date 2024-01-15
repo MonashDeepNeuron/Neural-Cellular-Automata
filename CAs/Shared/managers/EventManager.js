538ab419a071d2c6383577e6a021d9c59acd77ed
Nyan Kyaw <nkya0002@student.monash.edu>
2024-01-15 13:59:51 +1100

    small fix

--- CAs/Shared/managers/EventManager.js
+++ CAs/Shared/managers/EventManager.js
@@ -82,8 +82,8 @@ export default class EventManager {
         document.getElementById('play').addEventListener('click', EventManager.playPause);  // play pause button
         document.getElementById('next').addEventListener('click', EventManager.moveOneFrame); // move one frame button
         document.getElementsByTagName("body")[0].addEventListener("keydown", EventManager.keyListener); // key presses
-        document.getElementById('submitInput').addEventListener('click', EventManager.updateRuleString); // new rule string input button
-        document.getElementById('reset').addEventListener('click', EventManager.resetCanvas);
+        //document.getElementById('submitInput').addEventListener('click', EventManager.updateRuleString); // new rule string input button
+        //document.getElementById('reset').addEventListener('click', EventManager.resetCanvas);
         document.getElementById('speedInput').addEventListener('click', () => {
             EventManager.updateSpeed();
             clearInterval(EventManager.currentTimer);
