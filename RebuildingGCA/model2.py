f53cd0adebcc266963cb37734e0900d23a323dfe
chloekoee <chloe.koe@gmail.com>
2024-09-27 19:23:01 +1000

    Added GPU acceleration, debugged perception vector errors, added saving and loading of model weights and refactored code for clarity. also added requirements.txt and updated gitignore to include virtual environments

--- RebuildingGCA/model2.py
+++ RebuildingGCA/model2.py
@@ -10,10 +10,10 @@ class GCA(nn.Module):
     GRID_SIZE = 32
 
     def __init__(self, n_channels=16, hidden_channels=128):
-        # hidden channels are the number of channels in the linear layer in network
+        ## Hidden channels are the number of channels in the linear layer in network
         super().__init__()
 
-        # Represent the update step as a submodule
+        ## Represent the update step as a submodule
         self.update_network = (
             nn.Sequential(  # pytorch Conv2d layers automatically parallelize
                 nn.Conv2d(3 * n_channels, hidden_channels, kernel_size=1),
@@ -22,13 +22,15 @@ class GCA(nn.Module):
             )
         )
 
-        torch.nn.init.zeros_(self.update_network[0].weight)
-        torch.nn.init.zeros_(self.update_network[0].bias)
-        torch.nn.init.zeros_(self.update_network[2].weight)
-        
+        ## Initialise model parameters as much smaller numbers
+        torch.nn.init.normal_(self.update_network[0].weight, mean=0.0, std=0.001)
+        torch.nn.init.normal_(self.update_network[0].bias, mean=0.0, std=0.001)
+        torch.nn.init.normal_(self.update_network[2].weight, mean=0.0, std=0.001)
 
     def to(self, device):
-        """Move the model and constant tensors to the device (GPU)"""
+        """
+        Overrides parent nn.Module to method for sending resources to GPU
+        Move the model and constant tensors to the device (GPU)"""
         self.SOBEL_X = self.SOBEL_X.to(device)
         self.SOBEL_Y = self.SOBEL_Y.to(device)
         self.IDENTITY = self.IDENTITY.to(device)
@@ -36,12 +38,18 @@ class GCA(nn.Module):
 
     def forward(self, input_grid):
         """
-        1. Construct `perception_grid`
-        2. Apply update to each `perception_vector` in `perception_grid` to obtain `ds_grid`
-        3. Apply stochastic update mask to `ds_grid`
+        Input_grid is tensor with dims: (batch, in_channels, height, width)
+        1. Construct `perception_grid` by replacing each cell in input_grid with its feature vector
+        2. Apply update to each `perception_vector` in `perception_grid` to obtain `ds_grid`, the grid of changes
+        3. Apply stochastic update mask to `ds_grid` to filter out some changes
         4. Obtain next state of grid from `ds_grid` + `state_grid`
-        5. Apply alive cell masking to `state_grid`. This is the final updated state.
+        5. Apply alive cell masking to `state_grid` to kill of cells with alpha < 0.1
+        This yields output_filtered_grid, a tensor with dims: (batch, in_channels, height, width)
         """
+
+        ## Add input grid to the device model parameters are on
+        input_grid = input_grid.to(next(self.parameters()).device)
+
         perception_grid = self.calculate_perception_grid(input_grid)
         ds_grid = self.calculate_ds_grid(perception_grid)
         filtered_ds_grid = self.apply_stochastic_mask(ds_grid)
@@ -53,22 +61,25 @@ class GCA(nn.Module):
         """
         Calculates 1x48 perception vector for each cell in grid, returns as grid of perception vectors.
         Perception vectors are 4 dimensional. Unsqueeze used to add dimension of size 1 at index
-        
-        INPUTS: 
-            stategrid: Tensor wiith dims (batch, in_channels, height, width)
-
-            self (relevant components): 
-                SOBEL_X 
         """
-        # print(state_grid.size())
-        # print(self.SOBEL_X.unsqueeze(0).repeat(state_grid.size(1), 1, 1, 1).size())
-        grad_x = f.conv2d(state_grid, self.SOBEL_X.unsqueeze(0).repeat(state_grid.size(1), 1, 1, 1), stride=1, padding=1, groups = state_grid.size(1)) # TODO Replace padding with logic to make a grid that wraps around on itself.
-        grad_y = f.conv2d(state_grid, self.SOBEL_X.unsqueeze(0).repeat(state_grid.size(1), 1, 1, 1), stride=1, padding=1, groups = state_grid.size(1)) # TODO Replace 16 with a dynamic channels variable?
 
-        # print(grad_x.size())
+        grad_x = f.conv2d(
+            state_grid,
+            self.SOBEL_X.unsqueeze(0).repeat(state_grid.size(1), 1, 1, 1),
+            stride=1,
+            padding=1,
+            groups=state_grid.size(1),
+        )  # TODO Replace padding with logic to make a grid that wraps around on itself.
+        grad_y = f.conv2d(
+            state_grid,
+            self.SOBEL_Y.unsqueeze(0).repeat(state_grid.size(1), 1, 1, 1),
+            stride=1,
+            padding=1,
+            groups=state_grid.size(1),
+        )  # TODO Replace 16 with a dynamic channels variable?
 
         perception_grid = torch.cat([state_grid, grad_x, grad_y], dim=1)
-        # print(perception_grid.size())
+
         return perception_grid
 
     def calculate_ds_grid(self, perception_grid):
@@ -88,15 +99,12 @@ class GCA(nn.Module):
 
     def apply_alive_mask(self, state_grid):
         """Applies alive mask to state_grid
-        Does not use GPU to generate alive mask as doing max pooling"""
+        Does not use GPU to generate alive mask as doing max pooling
+        A cell is considered empty (set rgba : 0) if there is no mature (alpha > 0.1) cell in its 3x3 neighbourhood
+        """
         alive_mask = (
             f.max_pool2d(state_grid[:, 3:4, :, :], kernel_size=3, stride=1, padding=1)
             > 0.1
-        ) ## [:, 3:4, :, :] originally
-
-        ## a cell is considered empty (set rgba : 0) 
-        # if there is no mature (alpha > 0.1) cell in its 3x3 neighbourhood
-
-        # print(f"alive mask: {alive_mask.size()}")
+        )
 
         return alive_mask * state_grid
