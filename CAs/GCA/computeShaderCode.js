fe442e23253780c755bc3d996239dece74632c89
NyanKyaw <nkya0002@student.monash.edu>
2024-10-04 15:08:26 +1000

    Fixing Indexing Error in GCA - Bind groups being accessed in the wrong order in computeShaderCode.js. TODO: Implement wrap around training in gca_model.py.

--- CAs/GCA/computeShaderCode.js
+++ CAs/GCA/computeShaderCode.js
@@ -2,34 +2,24 @@ export const computeShaderCode =
 /*wgsl*/`
 @group(0) @binding(0) var<uniform> grid: vec2<f32>; 
 @group(0) @binding(1) var<storage> cellStateIn: array<f32>;
-@group(0) @binding(2) var<storage, read_write> cellStateOut: array<f32>; 
+@group(0) @binding(2) var<storage, read_write> cellStateOut: array<f32, 16>; 
 @group(0) @binding(3) var<storage> w1 : array<f32>;
-@group(0) @binding(4) var<storage> w2 : array<f32>;
-@group(0) @binding(5) var<storage> b1 : array<f32>;
+@group(0) @binding(4) var<storage> b1 : array<f32>;
+@group(0) @binding(5) var<storage> w2 : array<f32>;
 
 const WORKGROUP_SIZE: u32 = 16;
-
-/* Converts x y to scalar value. Represents first index of sixteen channels*/
-// fn cellIndex(cell: vec2<u32>) -> u32 {
-//     return ((cell.y % u32(grid.y)) * u32(grid.x) + (cell.x % u32(grid.x)))*16;
-// }
+const NUM_CHANNELS: u32 = 16;
 
 /* Converts x y to scalar value. Represents first index of sixteen channels
 Takes x, y, channel --> k (indexes cellstatein column vector) */
-fn cellIndex(cell: vec2<u32>) -> u32 {
-    return cell.y * u32(grid.x) * 16 + cell.x * 16;
+fn cellIndex(cell: vec2<u32>, channel: u32) -> u32 {
+    return (cell.y * u32(grid.x) + cell.x) * NUM_CHANNELS + channel;
 }
 
 
 fn cellValue(x: u32, y: u32, channel: u32) -> f32 {
-    return cellStateIn[cellIndex(vec2(x, y)) + channel];
+    return cellStateIn[cellIndex(vec2(x, y), channel)];
 }
-
-
-/* Returns index of channel of given cell*/
-// fn cellValue(x: u32, y: u32, channel: u32) -> f32 {
-//     return cellStateIn[cellIndex(vec2(x, y))+ channel];
-// }
 
 
 fn calculateSobelX(x: i32, y: i32, channel: i32) -> f32 {
@@ -43,6 +33,7 @@ fn calculateSobelX(x: i32, y: i32, channel: i32) -> f32 {
     }
     return sum;
 }
+
 
 fn calculateSobelY(x: i32, y: i32, channel: i32) -> f32 {
     let sobelFilterY: array<f32, 9> = array<f32, 9>(1.0, 2.0, 1.0, 0.0, 0.0, 0.0, -1.0, -2.0, -1.0);
@@ -64,9 +55,10 @@ fn relu(x: f32) -> f32 {
     return 0.0;
 }
 
+
 /* perception vector with contiguous identity + sobelx + sobel y */
 fn computePerceptionVector(x: i32, y: i32) -> array<f32, 48> {
-    var perceptionVector: array<f32, 48> = array<f32, 48>(); // x y identity (16 times)
+    var perceptionVector: array<f32, 48> = array<f32, 48>(); // [ Identity (len16), SobelX (len16), SobelY (len16)]
 
     // For each channel, first store the identity (original state)
     for (var j: i32 = 0; j <= 15; j = j + 1) {
@@ -86,6 +78,120 @@ fn computePerceptionVector(x: i32, y: i32) -> array<f32, 48> {
     return perceptionVector;
 }
 
+
+fn applyAliveMask(inputVector: array<f32, 16>, x: i32, y: i32) -> array<f32, 16> {
+    /*
+        A cell is inherently dead if there is no cells in its 3x3 
+        neighbourhood that are alive (channel 4 > 0.1) (alpha = index 3), return 
+        empty state vector as new state value
+    */
+
+    for (var i: i32 = -1; i <= 1; i++) {
+        for (var j: i32 = -1; j <= 1; j++) {
+            if (cellValue(u32(x + i), u32(y + j), 3u) > 0.1) {
+                return inputVector;
+            }
+        }
+    }
+
+    return array<f32, 16>(); 
+}
+
+
+fn computeLinearLayers(perceptionVector: array<f32, 48>, x: i32, y: i32) -> array<f32, 16> {
+        /*
+        
+        w1 = second linear layer weight vector 128 
+        b1 = second linear layer bias vector 128 
+        w2 = second linear layer weight vector 16
+
+        48 vec -> fully interconnected linear (w1) -> 128 vec -> add bias (b1)
+        -> 128 vec -> fully interconnected linear (w2) -> 16 vec (output)
+
+    */
+    var h1 = array<f32, 128>(); // First linear layer output
+    var h2 = array<f32, 16>(); // Initializes all elements to 1.0 (1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 
+    // 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0); 
+    // First linear layer (48 -> 128)
+    for (var i: u32 = 0u; i < 128u; i = i + 1u) {
+        for (var j: u32 = 0u; j < 48u; j = j + 1u) {
+            h1[i] += w1[i * 48u + j] * perceptionVector[j];
+        }
+        h1[i] = relu(h1[i] + b1[i]); 
+    }
+    
+    // Second linear layer (128 -> 16)
+    for (var i: u32 = 0u; i < 16u; i = i + 1u) {
+        for (var j: u32 = 0u; j < 128u; j = j + 1u) {
+            h2[i] += w2[i * 128u + j] * h1[j];
+        }
+    }
+
+    // for (var i: u32 = 0u; i < 16u; i = i + 1u) {
+    //     h2[i] = w2[126u+i];
+    // }
+
+    return h2;
+}
+
+fn applyStochasticMask(output_cell: array<f32, 16>, x: u32, y: u32) -> array<f32, 16> {
+    // Generate a pseudo-random value based on the cell position
+    let random_val = fract(sin(dot(vec2<f32>(f32(x), f32(y)), vec2<f32>(12.9898, 78.233))) * 43758.5453);
+
+    var ds_cell = output_cell;
+    // Apply mask with 50% chance on the entire vector for the current cell
+    if (random_val < 0.5) {
+        // Zero out the entire update vector for this cell
+        for (var i: u32 = 0u; i < 16u; i = i + 1u) {
+            ds_cell[i] = 0.0;  // Zero out the update for this cell
+        }
+    } 
+
+    return ds_cell;
+}
+
+@compute @workgroup_size(WORKGROUP_SIZE, WORKGROUP_SIZE)
+fn computeMain(@builtin(global_invocation_id) cell: vec3<u32>) {
+    let offset = cellIndex(cell.xy, 0); 
+
+    // Compute the perception vector
+    let perceptionVector = computePerceptionVector(i32(cell.x), i32(cell.y));
+
+    // Apply the update using linear layers
+    let output = computeLinearLayers(perceptionVector, i32(cell.x), i32(cell.y));
+
+    // Apply stochastic mask to the output
+    // let masked_output = applyStochasticMask(output, cell.x, cell.y);
+
+    // Calculate the final output (add the masked update to the current state)
+    var finalState: array<f32, 16> = array<f32, 16>();
+
+    // NYAN DIAGNOSIS -> output = [value , 0, 0, 0...,0]
+
+    for (var i: u32 = 0u; i < 16u; i = i + 1u) {
+        finalState[i] = cellStateIn[i + offset] + output[i]; // input_grid + filtered_ds_grid
+        //finalState[i] = cellStateIn[i] + output[i]; // input_grid + filtered_ds_grid
+    }
+
+    // Apply the alive mask to the entire final state
+    let finalOutput =  finalState; //applyAliveMask(finalState, i32(cell.x), i32(cell.y)); // output_filtered_grid
+
+    // Write the final output to cellStateOut
+    for (var i: u32 = 0u; i < 16u; i = i + 1u) {
+        cellStateOut[i + offset] = finalOutput[i];
+        // cellStateOut[i] = finalOutput[i];
+    }
+}
+
+/* Converts x y to scalar value. Represents first index of sixteen channels*/
+// fn cellIndex(cell: vec2<u32>) -> u32 {
+//     return ((cell.y % u32(grid.y)) * u32(grid.x) + (cell.x % u32(grid.x)))*16;
+// }
+
+/* Returns index of channel of given cell*/
+// fn cellValue(x: u32, y: u32, channel: u32) -> f32 {
+//     return cellStateIn[cellIndex(vec2(x, y))+ channel];
+// }
 
 /* perception vector with interleaved identity + sobelx + sobel y */
 // fn computePerceptionVector(x: i32, y: i32) -> array<f32, 48> {
@@ -118,106 +224,5 @@ fn computePerceptionVector(x: i32, y: i32) -> array<f32, 48> {
 //     }
 //     return ds_grid;
 // }
-
-fn applyAliveMask(inputVector: array<f32, 16>, x: i32, y: i32) -> array<f32, 16> {
-    /*
-        A cell is inherently dead if there is no cells in its 3x3 
-        neighbourhood that are alive (channel 4 > 0.1) (alpha = index 3), return 
-        empty state vector as new state value
-    */
-
-    for (var i: i32 = -1; i <= 1; i++) {
-        for (var j: i32 = -1; j <= 1; j++) {
-            if (cellValue(u32(x + i), u32(y + j), 3u) > 0.1) {
-                return inputVector;
-            }
-        }
-    }
-
-    return array<f32, 16>(); 
-}
-
-
-
-
-fn computeLinearLayers(perceptionVector: array<f32, 48>, x: i32, y: i32) -> array<f32, 16> {
-        /*
-        
-        w1 = second linear layer weight vector 128 
-        b1 = second linear layer bias vector 128 
-        w2 = second linear layer weight vector 16
-
-        48 vec -> fully interconnected linear (w1) -> 128 vec -> add bias (b1)
-        -> 128 vec -> fully interconnected linear (w2) -> 16 vec (output)
-
-    */
-    var h1 = array<f32, 128>(); // First linear layer output
-    var h2 = array<f32, 16>(); // Initializes all elements to 1.0 (1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 
-    // 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0); 
-    // First linear layer (48 -> 128)
-    for (var i: u32 = 0u; i < 128u; i = i + 1u) {
-        for (var j: u32 = 0u; j < 48u; j = j + 1u) {
-            h1[i] += w1[i * 48u + j] * perceptionVector[j];
-        }
-        h1[i] = relu(h1[i] + b1[i]); 
-    }
-    
-    // Second linear layer (128 -> 16)
-    for (var i: u32 = 0u; i < 16u; i = i + 1u) {
-        for (var j: u32 = 0u; j < 128u; j = j + 1u) {
-            h2[i] += w2[i * 128u + j] * h1[j];
-        }
-    }
-    
-    return h2;
-
-    // Apply stochastic mask to the output
-    // return applyStochasticMask(h2);
-}
-
-fn applyStochasticMask(output_cell: array<f32, 16>, x: u32, y: u32) -> array<f32, 16> {
-    // Generate a pseudo-random value based on the cell position
-    let random_val = fract(sin(dot(vec2<f32>(f32(x), f32(y)), vec2<f32>(12.9898, 78.233))) * 43758.5453);
-
-    var ds_cell = output_cell;
-    // Apply mask with 50% chance on the entire vector for the current cell
-    if (random_val < 0.5) {
-        // Zero out the entire update vector for this cell
-        for (var i: u32 = 0u; i < 16u; i = i + 1u) {
-            ds_cell[i] = 0.0;  // Zero out the update for this cell
-        }
-    } 
-
-    return ds_cell;
-}
-
-@compute @workgroup_size(WORKGROUP_SIZE, WORKGROUP_SIZE)
-fn computeMain(@builtin(global_invocation_id) cell: vec3<u32>) {
-    let offset = cellIndex(cell.xy); 
-
-    // Compute the perception vector
-    let perceptionVector = computePerceptionVector(i32(cell.x), i32(cell.y));
-
-    // Apply the update using linear layers
-    let output = computeLinearLayers(perceptionVector, i32(cell.x), i32(cell.y));
-
-    // Apply stochastic mask to the output
-    // let masked_output = applyStochasticMask(output, cell.x, cell.y);
-
-    // Calculate the final output (add the masked update to the current state)
-    var finalState: array<f32, 16> = array<f32, 16>();
-
-    for (var i: u32 = 0u; i < 16u; i = i + 1u) {
-        finalState[i] = cellStateIn[i + offset] + output[i]; // input_grid + filtered_ds_grid
-    }
-
-    // Apply the alive mask to the entire final state
-    let finalOutput =  applyAliveMask(finalState, i32(cell.x), i32(cell.y)); // output_filtered_grid
-
-    // Write the final output to cellStateOut
-    for (var i: u32 = 0u; i < 16u; i = i + 1u) {
-        cellStateOut[i + offset] = finalOutput[i];
-    }
-}
 
 `
