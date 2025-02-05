a4920cc661dc12f169e4bb37d5d03d7c16d7ebb4
AngaBlue <contact@anga.blue>
2025-02-06 00:59:06 +1100

    fix: throw on non 2xx status code

--- web/util/loadWeights.ts
+++ web/util/loadWeights.ts
@@ -4,6 +4,7 @@
  */
 export default async function loadWeights(url: string): Promise<Float32Array> {
 	const res = await fetch(url);
+	if (!res.ok) throw new Error(`${res.status}: ${res.statusText}`);
 	const buffer = await res.arrayBuffer();
 	return new Float32Array(buffer);
 }
