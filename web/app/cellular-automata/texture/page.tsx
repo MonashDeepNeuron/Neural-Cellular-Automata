a22b2a678feeaecf83ba3a6f6a6c9b037565f5ce
AngaBlue <contact@anga.blue>
2025-02-06 13:49:14 +1100

    feat: prettify control panel

--- web/app/cellular-automata/texture/page.tsx
+++ web/app/cellular-automata/texture/page.tsx
@@ -4,7 +4,7 @@ import Card from '@/components/Card';
 import FramerateSlider from '@/components/FramerateSlider';
 import clsx from 'clsx';
 import simulation from './simulation';
-import useNCA from './useNCA';
+import useNCA, { NCAStatus } from './useNCA';
 
 const SIZE = 512;
 
@@ -25,16 +25,21 @@ export default function Texture() {
 			<h1>Texture</h1>
 			<div className='flex gap-4 flex-row'>
 				<Card>
+					<p>
+						<b>Status</b>: {status}
+					</p>
+					<p>
+						<b>Step</b>: {step}
+					</p>
+					<FramerateSlider state={FPS} setState={setFPS} />
 					<button
 						type='button'
-						className={clsx('px-4 py-2 text-white min-w-24 rounded-md', play ? 'bg-red-700' : 'bg-green-700')}
+						disabled={status !== NCAStatus.READY}
+						className={clsx('mt-4 px-4 py-2 text-white min-w-24 rounded-md font-bold', play ? 'bg-red-700' : 'bg-green-700')}
 						onClick={() => setPlay(!play)}
 					>
 						{play ? 'Pause' : 'Play'}
 					</button>
-					<p><b>Status</b>: {status}</p>
-					<p><b>Step</b>: {step}</p>
-					<FramerateSlider state={FPS} setState={setFPS} />
 				</Card>
 				<Card className='relative inline-block'>
 					<div className='absolute top-0 left-0 flex items-center justify-center overflow-hidden w-full h-full'>
--- web/components/FramerateSlider.tsx
+++ web/components/FramerateSlider.tsx
@@ -13,9 +13,9 @@ export default function FramerateSlider({ state, setState, min = 1, max = 120 }:
 	const id = useId();
 
 	return (
-		<div className='mt-4 flex flex-col items-center bg-background p-8 w-96'>
-			<label htmlFor={id} className='text-md font-semibold mb-2'>
-				Frames Per Second
+		<div className='w-96 max-w-full'>
+			<label htmlFor={id} className='text-md mb-2'>
+				<b>Framerate</b>:
 			</label>
 			<input
 				id={id}
@@ -24,9 +24,9 @@ export default function FramerateSlider({ state, setState, min = 1, max = 120 }:
 				max={max}
 				value={state}
 				onChange={e => setState(Number(e.target.value))}
-				className='w-full max-w-md'
+				className='w-full'
 			/>
-			<div className='flex justify-between w-full max-w-md text-sm mt-2'>
+			<div className='flex justify-between w-full text-sm mt-2'>
 				<span>{min} FPS</span>
 				<span>{state} FPS</span>
 				<span>{max} FPS</span>
