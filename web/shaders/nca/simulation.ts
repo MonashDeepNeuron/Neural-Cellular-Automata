1068b1efe90bd27464365104ddab39bb8930fb0b
AngaBlue <contact@anga.blue>
2025-02-27 01:17:48 +1100

    feat: persisting gca

--- web/shaders/nca/simulation.ts
+++ web/shaders/nca/simulation.ts
@@ -52,7 +52,7 @@ fn compute_main(@builtin(global_invocation_id) pos: vec3u) {
 
   // Copy identity convolution directly from state
   for (var c = 0u; c < CHANNELS; c++) {
-    perceptions[c * 4 + 0] = state[index(c, x, y)];
+    perceptions[c * CONVOLUTIONS + 0] = state[index(c, x, y)];
   }
 
   // Compute convolutions
@@ -128,8 +128,8 @@ fn alive_mask(x: u32, y: u32) {
       // Find neighbours with circular padding
       let dx = (x + kx - 1 + size) % size;
       let dy = (y + ky - 1 + size) % size;
-      let i = index(4, dx, dy);
-      if (next_state[i] > 0.1) {
+      // Check alpha > 0.1
+      if (next_state[index(3, dx, dy)] > 0.1) {
         return;
       }
     }
@@ -156,7 +156,7 @@ export const texture = nca({
 	aliveMasking: false
 });
 
-export const gca = nca({
+export const persisting = nca({
 	convolutions: [SOBEL_X, SOBEL_Y],
 	channels: 16,
 	hiddenChannels: 128,
