958c387db30a7b34e353da096e6f3d9a6c3f675f
AngaBlue <contact@anga.blue>
2025-02-27 20:49:38 +1100

    feat: centering

--- web/app/cellular-automata/texture/page.tsx
+++ web/app/cellular-automata/texture/page.tsx
@@ -6,7 +6,7 @@ import useNCA, { NCAStatus } from '@/hooks/useNCA';
 import { texture as simulation } from '@/shaders/nca/simulation';
 import clsx from 'clsx';
 
-const SIZE = 512;
+const SIZE = 256;
 
 export default function Texture() {
 	const { error, status, play, setPlay, step, FPS, setFPS, canvasRef } = useNCA({
@@ -21,33 +21,33 @@ export default function Texture() {
 	});
 
 	return (
-		<div>
-			<h1>Texture</h1>
-			<div className='flex gap-4 flex-row'>
-				<Card>
-					<p>
-						<b>Status</b>: {status}
-					</p>
-					<p>
-						<b>Step</b>: {step}
-					</p>
-					<FramerateSlider state={FPS} setState={setFPS} max={240} />
-					<button
-						type='button'
-						disabled={status !== NCAStatus.READY}
-						className={clsx('mt-4 px-4 py-2 text-white min-w-24 rounded-md font-bold', play ? 'bg-red-700' : 'bg-green-700')}
-						onClick={() => setPlay(!play)}
-					>
-						{play ? 'Pause' : 'Play'}
-					</button>
-				</Card>
-				<Card className='relative inline-block'>
+		<div className='grid gap-4 grid-rows-2 grid-cols-1 max-w-full lg:grid-rows-1 lg:grid-cols-[24rem,1fr] lg:h-[calc(100vh-6rem)]'>
+			<Card>
+				<h1>Texture</h1>
+				<p>
+					<b>Status</b>: {status}
+				</p>
+				<p>
+					<b>Step</b>: {step}
+				</p>
+				<FramerateSlider state={FPS} setState={setFPS} max={240} />
+				<button
+					type='button'
+					disabled={status !== NCAStatus.READY}
+					className={clsx('mt-4 px-4 py-2 text-white min-w-24 rounded-md font-bold', play ? 'bg-red-700' : 'bg-green-700')}
+					onClick={() => setPlay(!play)}
+				>
+					{play ? 'Pause' : 'Play'}
+				</button>
+			</Card>
+			<Card className='flex justify-center'>
+				<div className='relative aspect-square h-full max-h-full max-w-full overflow-hidden'>
 					<div className='absolute top-0 left-0 flex items-center justify-center overflow-hidden w-full h-full'>
 						{error && <p className='text-red-500 px-4 text-center'>{error}</p>}
 					</div>
-					<canvas height={SIZE} width={SIZE} ref={canvasRef} />
-				</Card>
-			</div>
+					<canvas height={SIZE} width={SIZE} className='h-full w-full' ref={canvasRef} />
+				</div>
+			</Card>
 		</div>
 	);
 }
