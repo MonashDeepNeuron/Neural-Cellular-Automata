e74c2c3e9626aca7f2fed255080c647ba01d8090
AngaBlue <contact@anga.blue>
2025-03-01 00:03:41 +1100

    chore: fix font import

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,26 +1,21 @@
-import { Geist, Geist_Mono, Poppins } from 'next/font/google';
+import { Poppins } from 'next/font/google';
 import './globals.css';
 import Providers from '@/components/Providers';
 import Navbar from '@/components/layout/Nav';
 import createMetadata from '@/util/createMetadata';
 import clsx from 'clsx';
+import type { Viewport } from 'next';
 
 const poppins = Poppins({
-	weight: ["500","400"], 
-	subsets: ['latin']
-})
-
-const geistSans = Geist({
-	variable: '--font-geist-sans',
-	subsets: ['latin']
-});
-
-const geistMono = Geist_Mono({
-	variable: '--font-geist-mono',
+	weight: ['500', '400'],
 	subsets: ['latin']
 });
 
 export const metadata = createMetadata();
+
+export const viewport: Viewport = {
+	themeColor: '#7065f9'
+};
 
 export default function RootLayout({
 	children
@@ -29,7 +24,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='en'>
-			<body className={`${poppins.className}`}>
+			<body className={clsx(poppins.className, 'antialiased text-black overflow-x-hidden max-w-screen')}>
 				<Navbar />
 				<main className='min-h-screen p-4 pt-20 max-w-full'>
 					{/* Added 'pt-20' for top padding */}
