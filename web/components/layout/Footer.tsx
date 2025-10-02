1a0cc41134a0964047890d4abf15cc1398fabe59
amaiable <atmai2002@gmail.com>
2025-10-02 17:19:48 +1000

    feat: implemented simulator page for selecting model for simulation

--- web/components/layout/Footer.tsx
+++ web/components/layout/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
 	return (
 		<footer>
-			<p>&copy; 2024 Neural Cellular Automata. All rights reserved.</p>
+			<p>&copy; 2025 Neural Cellular Automata. All rights reserved.</p>
 		</footer>
 	);
 }
